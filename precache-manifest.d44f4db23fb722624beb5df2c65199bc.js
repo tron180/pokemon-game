84715aa6a63a2745097a29ee60e672d012234036
Yash Mishra <yash.mishra@raweng.com>
2019-05-29 15:31:06 +0530

    Updates

---
+++ precache-manifest.d44f4db23fb722624beb5df2c65199bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19e6a4ca1367b5c1be90e46d55dce13",
+    "url": "/pokemon-game/index.html"
+  },
+  {
+    "revision": "83943bfa2c8a50118c8f",
+    "url": "/pokemon-game/static/css/main.0c1a2653.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokemon-game/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "83943bfa2c8a50118c8f",
+    "url": "/pokemon-game/static/js/main.318a68eb.chunk.js"
+  },
+  {
+    "revision": "027869921b0514aae066",
+    "url": "/pokemon-game/static/js/runtime~main.ec403106.js"
+  }
+]);
